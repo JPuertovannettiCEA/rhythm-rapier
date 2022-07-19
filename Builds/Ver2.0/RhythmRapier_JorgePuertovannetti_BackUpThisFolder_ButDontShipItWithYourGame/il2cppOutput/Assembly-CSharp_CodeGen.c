4d1a186330beafdb92dacd43fd65385687b466b1
Jorge <jorge@925.gg>
2022-07-18 17:23:48 -0700

    Uploaded second build for testing

---
+++ Builds/Ver2.0/RhythmRapier_JorgePuertovannetti_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,113 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void ChangeScene::StartButton()
+extern void ChangeScene_StartButton_mAF6E33E62FDEEE7B8C778F1DF80911C57FC82E8C (void);
+// 0x00000002 System.Void ChangeScene::QuitButton()
+extern void ChangeScene_QuitButton_m6B5D85D6E3DC1923B06BA84F7F109A67E83A30C5 (void);
+// 0x00000003 System.Void ChangeScene::.ctor()
+extern void ChangeScene__ctor_m31CB879A0944B416D16AA2130F9504E1ABAD6FA1 (void);
+// 0x00000004 System.Void FinalMessage::Start()
+extern void FinalMessage_Start_mDA4758714111FC42BFDFF3ABAA68F85E0E3EA942 (void);
+// 0x00000005 System.Void FinalMessage::Update()
+extern void FinalMessage_Update_m9CEF1B961EC2673C49857BFD3CABC0DE5DC5E51D (void);
+// 0x00000006 System.Void FinalMessage::.ctor()
+extern void FinalMessage__ctor_m363C95B0A193689CE0C9351E3CF82F0CA0F70B6B (void);
+// 0x00000007 System.Void MovementCube::Start()
+extern void MovementCube_Start_m3979E421A015E2289754A4E817453E6B0E40440D (void);
+// 0x00000008 System.Void MovementCube::Update()
+extern void MovementCube_Update_m5CB8DB88C81ED7E4F3B5F722AE1FF9D326BB71B4 (void);
+// 0x00000009 System.Void MovementCube::.ctor()
+extern void MovementCube__ctor_mB5B5413DE5C52141EE70FC38B8BF2E5AAE053771 (void);
+// 0x0000000A System.Void Rapier::Start()
+extern void Rapier_Start_m8F1C0262CD80591751F62E98E6168212992517DD (void);
+// 0x0000000B System.Void Rapier::Update()
+extern void Rapier_Update_m37EBC946B228B192D0654EE309E5CF036B1C78B4 (void);
+// 0x0000000C System.Void Rapier::.ctor()
+extern void Rapier__ctor_mFC70F5A0517CF7EDA678B17CC56B3F24470A4556 (void);
+// 0x0000000D System.Void ScoreManager::Start()
+extern void ScoreManager_Start_m7E00370AD0134DB3AEA647B9BA69055725AA83FE (void);
+// 0x0000000E System.Void ScoreManager::Update()
+extern void ScoreManager_Update_m68EC29B04B4118B51E293C1C3077D87FF3FE7811 (void);
+// 0x0000000F System.Void ScoreManager::.ctor()
+extern void ScoreManager__ctor_mE5C264C34A9CBBAB72DCF99CCCC9841AF8F9FEE6 (void);
+// 0x00000010 System.Void Spawner::Start()
+extern void Spawner_Start_m8BB68F2141CF386C08091E93308438F56B08EE73 (void);
+// 0x00000011 System.Void Spawner::Update()
+extern void Spawner_Update_m6DAC21E4E2E5A3C46C3F3EE45A3358909808DDC5 (void);
+// 0x00000012 System.Void Spawner::.ctor()
+extern void Spawner__ctor_mBF592E8E9B5682687D8C28E73A64BF29B6BF2088 (void);
+static Il2CppMethodPointer s_methodPointers[18] = 
+{
+	ChangeScene_StartButton_mAF6E33E62FDEEE7B8C778F1DF80911C57FC82E8C,
+	ChangeScene_QuitButton_m6B5D85D6E3DC1923B06BA84F7F109A67E83A30C5,
+	ChangeScene__ctor_m31CB879A0944B416D16AA2130F9504E1ABAD6FA1,
+	FinalMessage_Start_mDA4758714111FC42BFDFF3ABAA68F85E0E3EA942,
+	FinalMessage_Update_m9CEF1B961EC2673C49857BFD3CABC0DE5DC5E51D,
+	FinalMessage__ctor_m363C95B0A193689CE0C9351E3CF82F0CA0F70B6B,
+	MovementCube_Start_m3979E421A015E2289754A4E817453E6B0E40440D,
+	MovementCube_Update_m5CB8DB88C81ED7E4F3B5F722AE1FF9D326BB71B4,
+	MovementCube__ctor_mB5B5413DE5C52141EE70FC38B8BF2E5AAE053771,
+	Rapier_Start_m8F1C0262CD80591751F62E98E6168212992517DD,
+	Rapier_Update_m37EBC946B228B192D0654EE309E5CF036B1C78B4,
+	Rapier__ctor_mFC70F5A0517CF7EDA678B17CC56B3F24470A4556,
+	ScoreManager_Start_m7E00370AD0134DB3AEA647B9BA69055725AA83FE,
+	ScoreManager_Update_m68EC29B04B4118B51E293C1C3077D87FF3FE7811,
+	ScoreManager__ctor_mE5C264C34A9CBBAB72DCF99CCCC9841AF8F9FEE6,
+	Spawner_Start_m8BB68F2141CF386C08091E93308438F56B08EE73,
+	Spawner_Update_m6DAC21E4E2E5A3C46C3F3EE45A3358909808DDC5,
+	Spawner__ctor_mBF592E8E9B5682687D8C28E73A64BF29B6BF2088,
+};
+static const int32_t s_InvokerIndices[18] = 
+{
+	5387,
+	5387,
+	5387,
+	5387,
+	5387,
+	5387,
+	5387,
+	5387,
+	5387,
+	5387,
+	5387,
+	5387,
+	5387,
+	5387,
+	5387,
+	5387,
+	5387,
+	5387,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	18,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
